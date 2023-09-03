4b0a872077c2788abdeb46fcc702a2ebaba29b81
xavier <xmoore122@gmail.com>
2023-09-03 08:47:24 -0600

    hmm

--- include/config.h
+++
@@ -1,6 +0,0 @@
-/* Automatically generated - do not edit */
-#define CFG_BOARDDIR board/hisilicon/hikey
-#include <configs/hikey.h>
-#include <asm/config.h>
-#include <linux/kconfig.h>
-#include <config_fallbacks.h>
